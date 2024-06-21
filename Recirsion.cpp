8b11c265a6435492532106d0c742e8b56bbbc72a
kunalArya1 <kunalkrraj220@gmail.com>
2024-06-21 16:07:27 +0530

    Some Question Added

--- Recirsion.cpp
+++ Recirsion.cpp
@@ -15,11 +15,27 @@ bool searchTargetInAraay(int arr[],int target,int index,int size){
    return ans;
 }
 
+
+int maxInArray(int arr[],int index,int size){
+    int max= 0;
+    if(index == size){
+        return;
+    }
+
+    if( arr[index] > max){
+        max = arr[index];
+    }
+
+    int maxNum= maxInArray(arr,index+1,size);
+    return maxNum;
+}
+
 int main(){
     int target = 90;
     int index =0;
     int size = 6;
     int arr[] = {10,20,30,40,50,60};
-    bool value= searchTargetInAraay(arr,target,index,size);
+    // bool value= searchTargetInAraay(arr,target,index,size);
+    int value = maxInArray(arr,index,size);
     cout<<value<<endl;
 }
